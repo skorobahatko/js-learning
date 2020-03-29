d1e42895a12530d29ea31a15b71d65252573aebd
skorobahatko <maxim.skorobagatko@gmail.com>
2020-03-29 12:34:36 +0300

    homeworkTwoJS2 added

---
+++ js-home/calculator/script.js
@@ -0,0 +1,11 @@
+// let i = 0;
+// let j = 0;
+// for (i = 0; i < 60; i++ ) {
+//     document.write(`${i} second <br>`)
+//     if (i === 59) {
+//         document.write('hello');
+//         // for (j = 0; j < 24; j++){
+//             // do
+//         // }
+//     }
+// }
