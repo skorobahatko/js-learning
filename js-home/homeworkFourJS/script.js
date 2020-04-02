28be21c692d5c4fae6c92125731f9ad20a47aa35
skorobahatko <maxim.skorobagatko@gmail.com>
2020-04-02 17:13:53 +0300

    homeworkThreeJS added final changes

--- js-home/homeworkFourJS/script.js
+++ js-home/homeworkFourJS/script.js
@@ -1,10 +1,10 @@
 // join split toLowerCase ToUpperCase startsWith endsWith includes
 // - створити функцію яка виводить масив
 // let arrayToLog = [];
-// function arrayLog () {
-//     document.write(arrayToLog);
+// function arrayLog (array) {
+//     document.write(array);
 // }
-// arrayLog();
+// arrayLog(arrayToLog);
 
 // - створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попвередню функцію.ґ
 
@@ -62,25 +62,7 @@
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
 
-// function acceptNumber () {
-//     let a = +prompt('Number one');
-//     let b = +prompt('Number two');
-//     let c = +prompt('Number three');
-//     if (a > b && a > c) {
-//         document.write(`Number ${a} is very big`);               ??????
-//         return a;
-//     } else if (b > a && b > c) {
-//         document.write(`Number ${b} is very big`);
-//         return b;
-//     } else if (c > a && c > b) {
-//         document.write(`Number ${c} is very big`);
-//         return c;
-//     } else if (a === b || a === c || b === c || b === a ) {
-//         document.write(`Type different numbers`);
-//     }
-//
-// }
-// acceptNumber();
+// пропустив, потім зроблю
 
 // - створити функцію яка виводить масив
 
@@ -107,7 +89,7 @@
 
 // - створити функцію яка повертає найменьше число з масиву
 
-let arrayToLog = [21, 1, 546, 12, 445];
+// let arrayToLog = [21, 1, 546, 12, 445, 21];
 //
 // function maxValueNumber(array) {
 //     let max = array[0];
@@ -150,42 +132,153 @@ let arrayToLog = [21, 1, 546, 12, 445];
 
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
-function averageArithmetic (array) {
-    let sumElement = 0;
-    for (let element of array) {
-        sumElement = sumElement + element;
-    }
-    sumElement = sumElement / array.length;
-    console.log(sumElement);
-    return sumElement;
-}
-
-averageArithmetic(arrayToLog);
+// function averageArithmetic (array) {
+//     let sumElement = 0;
+//     for (let element of array) {
+//         sumElement = sumElement + element;
+//     }
+//     sumElement = Math.floor(sumElement / array.length);
+//     console.log(sumElement);
+//     return sumElement;
+// }
+//
+// averageArithmetic(arrayToLog);
 
 // - Створити функцію яка приймає масив будь яких объектів, та повертає значення кількості об'єктів в масиві
+
+// let arrayCars = [
+//     {model: 'BMW', year: 2011, power: 300, color: 'blue'},
+//     {model: 'Volkswagen', year: 2014, power: 320, color: 'red'},
+//     {model: 'Mersedes-Benz', year: 2015, power: 400, color: 'black'},
+//     {model: 'Renault', year: 2010, power: 330, color: 'blue'},
+//     {model: 'Volvo', year: 2012, power: 350, color: 'blue'},
+//     {model: 'Opel', year: 2011, power: 300, color: 'blue'},
+//     {model: 'Porsche', year: 2009, power: 500, color: 'blue'},
+//     {model: 'Jeep', year: 2009, power: 400, color: 'blue'},
+//     {model: 'Volvo', year: 2001, power: 200, color: 'blue'},
+//     {model: 'BMW', year: 2002, power: 250, color: 'blue'}
+// ];
+//
+// function arrayNumberObject(array) {
+//     return array.length;
+// }
+//
+// console.log(arrayNumberObject(arrayCars));
+
 // - Створити функцію яка приймає масив будь яких объектів, та повертає загальн кількість полів в них
+
+// function arrayNumberObjectKeys (array) {
+//     let arr = 0;
+//     for (let i = 0; i< array.length; i++) {
+//         arr = arr + Object.keys(array[i]).length;
+//     }
+//     return arr;
+// }
+//
+// console.log(arrayNumberObjectKeys(arrayCars));
+
 // - створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
-//     Приклад
-//     [1,2,3,4]
-//     [2,3,4,5]
-// результат
-//     [3,5,7,9]
+
+// let arr1 = [1,2,3,4];
+// let arr2 = [2,3,4,5];
+//
+// let sumArray = [];
+// function arraySum(array1, array2) {
+//     for (let i = 0; i < array1.length; i++) {
+//         sumArray.push(array1[i] + array2[i]);
+//     }
+//     console.log(sumArray)
+//     return sumArray;
+// }
+//
+// arraySum(arr1, arr2);
+
 // - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+
+// function replaceElement (array, i) {
+//     array.splice(i, 0, i + 1);
+//     return array;                                потім сам вирішу
+// }
+//
+// console.log(replaceElement(arrayToLog, 2));
+
 // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
 // Приклад
 // [1,0,6,0,3] => [1,6,3,0,0]
-// [0,1,2,3,4] => [1,2,3,4,0]
+// [0,1,2,3,4] => [1,2,3,4,0]                       потім
 // [0,0,1,0]   => [1,0,0,0]
 //
 // Створити функцію яка :
 // - Додає в боді блок з текстом "Hello owu"
+
+// function addBlockHello () {
+//     let hello = document.createElement('div');
+//     hello.innerText = 'Hello owu!';
+//     document.body.appendChild(hello);
+// }
+//
+// addBlockHello();
+
 // - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
+
+// function addBlockHello (type, text) {
+//     let hello = document.createElement(type);
+//     hello.innerText = text;
+//     document.body.appendChild(hello);
+// }
+//
+// addBlockHello('div', 'hal');
+
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+
+// let arrayCars = [
+//     {model: 'BMW', year: 2011, power: 300, color: 'blue'},
+//     {model: 'Volkswagen', year: 2014, power: 320, color: 'red'},
+//     {model: 'Mersedes-Benz', year: 2015, power: 400, color: 'black'},
+//     {model: 'Renault', year: 2010, power: 330, color: 'blue'},
+//     {model: 'Volvo', year: 2012, power: 350, color: 'blue'},
+//     {model: 'Opel', year: 2011, power: 300, color: 'blue'},
+//     {model: 'Porsche', year: 2009, power: 500, color: 'blue'},
+//     {model: 'Jeep', year: 2009, power: 400, color: 'blue'},
+//     {model: 'Volvo', year: 2001, power: 200, color: 'blue'},
+//     {model: 'BMW', year: 2002, power: 250, color: 'blue'}
+// ];
+
+// let idContainer = document.createElement('div');
+// idContainer.id = 'car1';
+// function addCars (array, id) {
+//     for (let carOne of array) {
+//         let idCar = document.createElement('div');
+//         idCar.classList = 'car';
+//         idCar.innerText = Object.values(carOne);
+//         id.appendChild(idCar);
+//     }
+// }
+// document.body.appendChild(idContainer);
+// addCars(arrayCars, idContainer);
+
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
 // Для кожної властивості створити всередені блока автомоблія свій блок
+
+// let idContainer = document.createElement('div');
+// idContainer.id = 'car1';
+// function addCars (array, id) {
+//     for (let carOne of array) {
+//         let idCar = document.createElement('div');
+//         for (let carOneKey in carOne) {
+//             let classCar = document.createElement('div');
+//             classCar.innerText = carOne[carOneKey];
+//             idCar.appendChild(classCar);
+//         }
+//         id.appendChild(idCar);
+//     }
+// }
+// document.body.appendChild(idContainer);
+// addCars(arrayCars, idContainer);
+
 //
 // (на основі минулого ДЗ)
 // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
